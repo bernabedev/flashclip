f0503b5c8280429e278430f1baa38cf844e22e04
Freilyn Bernabe <freilyn@bernabe.dev>
2025-05-15 09:13:24 -0400

    feat: Implement header component and ninja mode alert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
 import GetClip from "@/components/get_clip";
 import Footer from "@/components/layout/footer";
-import LogoHeader from "@/components/layout/logo-header";
-import { Button } from "@/components/ui/button";
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
-import { UserIcon, ZapIcon } from "lucide-react";
+import Header from "@/components/layout/header";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { SignedOut } from "@clerk/nextjs";
+import { HandMetalIcon } from "lucide-react";
 
 export default function Home() {
   return (
@@ -17,36 +11,19 @@ export default function Home() {
       <div className="fixed -z-10 h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]"></div>
       <div className="fixed -z-10 left-1/2 transform -translate-x-1/2 max-w-7xl 2xl:w-[calc(100%-16rem)] xl:w-full w-[calc(100%-3.5rem)] h-screen border border-dashed border-slate-200/90"></div>
       <div className="min-h-14 border border-dashed border-slate-200/90">
-        <header className="max-w-7xl mx-auto h-14 flex items-center px-8 py-4 justify-between">
-          <LogoHeader />
-          <div className="flex items-center gap-2">
-            <SignedOut>
-              <SignInButton>
-                <Button variant="outline" className="min-w-32">
-                  <UserIcon className="size-4" />
-                  Sign In
-                </Button>
-              </SignInButton>
-              <SignUpButton>
-                <Button className="min-w-32">
-                  <ZapIcon className="size-4" />
-                  Get Started
-                </Button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton
-                appearance={{
-                  elements: {
-                    userButtonAvatarBox: "!size-8",
-                  },
-                }}
-              />
-            </SignedIn>
-          </div>
-        </header>
+        <Header />
       </div>
       <main className="flex flex-1 flex-col max-w-xl mx-auto w-full px-8 py-4 justify-center items-center">
+        <SignedOut>
+          <Alert className="mb-10">
+            <HandMetalIcon className="h-4 w-4 !text-primary" />
+            <AlertTitle>You&apos;re in ninja mode!</AlertTitle>
+            <AlertDescription className="text-black/50 text-xs">
+              No login, no trace. Log in to save clips and access your dashboard
+              powers.
+            </AlertDescription>
+          </Alert>
+        </SignedOut>
         <GetClip />
       </main>
       <Footer />
--- src/components/layout/user-nav-header.tsx
+++ src/components/layout/user-nav-header.tsx
@@ -1,16 +1,36 @@
 "use client";
 import { cn } from "@/lib/utils";
-import { SignedIn, UserButton } from "@clerk/nextjs";
-import { LayoutDashboard } from "lucide-react";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+import { LayoutDashboard, UserIcon, ZapIcon } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { buttonVariants } from "../ui/button";
+import { Button, buttonVariants } from "../ui/button";
 
 export default function UserNavHeader() {
   const pathname = usePathname();
   const isDashboard = pathname.startsWith("/dashboard");
   return (
     <div className="flex items-center gap-4">
+      <SignedOut>
+        <SignInButton>
+          <Button variant="outline" className="min-w-32">
+            <UserIcon className="size-4" />
+            Sign In
+          </Button>
+        </SignInButton>
+        <SignUpButton>
+          <Button className="min-w-32">
+            <ZapIcon className="size-4" />
+            Get Started
+          </Button>
+        </SignUpButton>
+      </SignedOut>
       <SignedIn>
         <UserButton
           fallback={
